a5aba4e6b2da2413267bf6316926ec9764711d92
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-03 19:35:10 +0300

    new function of get mask account

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number("Maestro 1596837868705199"))
+    print(get_mask_card_number("Visa Gold 5999414228426353"))
     print(get_mask_account("Счет 73654108430135874305"))
