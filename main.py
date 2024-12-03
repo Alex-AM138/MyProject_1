72eabb00b3accfebaba90092e1d76b13c880031c
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-03 19:31:19 +0300

    new function of get mask card number

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-    print(get_mask_account("73654108430135874305"))
+    print(get_mask_card_number("Maestro 1596837868705199"))
+    print(get_mask_account("Счет 73654108430135874305"))
--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 # Реализация первой функции
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты, заменяя цифры на '*'"""
-    return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    return card_number[-16:-12] + " " + card_number[-12:-10] + "** **** " + card_number[-4:]
 
 
 # Реализация второй функции
