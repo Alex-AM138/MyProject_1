250207c98573b7a4485bd076a9b9d3de83bd11d0
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-03 20:44:41 +0300

    result of exercises

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,14 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(unidentified_string: str) -> str:
-    if 'Счет' in unidentified_string:
+    """Функция принимает строку и маскирует ее в зависимости от данных строки"""
+    if "Счет" in unidentified_string:
         return get_mask_account(unidentified_string)
     else:
-        return  get_mask_card_number(unidentified_string)
+        return get_mask_card_number(unidentified_string)
 
 
 def get_date(date_string: str) -> str:
-    return date_string[8:10] + '.' + date_string[5:7] + '.' + date_string[:4]
+    """Функция принимает строку и возвращает строку в формате 'ДД.ММ.ГГГГ'"""
+    return date_string[8:10] + "." + date_string[5:7] + "." + date_string[:4]
