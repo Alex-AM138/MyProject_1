19c6a7c2c2e21592dc05002bf765d35cbc5675bc
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-10 21:35:53 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,21 @@
-def filter_by_state(list_of_dict: list, status_for_key: str = 'EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list, status_for_key: str = 'EXECUTED') -> list:
     """Функция принимает список словарей и выводит новый список словарей, по указанному ключу 'state',
         если ключ не был дан на вход функции, будет выведен список по ключу по умолчанию"""
     new_list = []
     if status_for_key == 'EXECUTED':
-        for state in list_of_dict:
+        for state in list_of_dicts:
             if state.get('state') == 'EXECUTED':
                 new_list.append(state)
         return new_list
     elif status_for_key == 'CANCELED':
-        for state in list_of_dict:
+        for state in list_of_dicts:
             if state.get('state') == 'CANCELED':
                 new_list.append(state)
         return new_list
-# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
 
-def sort_by_date():
-    pass
+
+def sort_by_date(list_of_dicts: list, ascending: bool= True) -> list:
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+        (по умолчанию - убывание) и возвращает новый список отсортированный по дате"""
+    new_list = [sorted(list_of_dicts, key=lambda date: date.get('date', 0), reverse=ascending)]
+    return new_list
