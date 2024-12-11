1200be3b5cf0a1108ea7ca7c9776946f3e9ca12f
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-11 09:43:04 +0300

    recreated a new thread feature/homework_10_1, fixes main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# from src.masks import get_mask_card_number, get_mask_account
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
-    # print(get_mask_card_number("Visa Gold 5999414228426353"))
-    # print(get_mask_account("Счет 73654108430135874305"))
+
     print(mask_account_card("Visa Platinum 7000792289606361"))
-    # print(mask_account_card("Счет 73654108430135874305"))
+
     print(get_date("2024-03-11T02:26:18.671407"))
     print(
         filter_by_state(
