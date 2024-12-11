8baf7a2f8fa5f55a3e4278b00fb5e7db749a285c
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-11 09:17:16 +0300

    fixes function filter_by_state

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # from src.masks import get_mask_card_number, get_mask_account
-from src.widget import mask_account_card, get_date
-
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
-  # print(get_mask_card_number("Visa Gold 5999414228426353"))
-  # print(get_mask_account("Счет 73654108430135874305"))
-    print(mask_account_card('Visa Platinum 7000792289606361'))
-  # print(mask_account_card("Счет 73654108430135874305"))
+    # print(get_mask_card_number("Visa Gold 5999414228426353"))
+    # print(get_mask_account("Счет 73654108430135874305"))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    # print(mask_account_card("Счет 73654108430135874305"))
     print(get_date("2024-03-11T02:26:18.671407"))
--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,15 @@
-def filter_by_state(list_of_dicts: list, status_for_key: str = 'EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list, status_for_key: str = "EXECUTED") -> list:
     """Функция принимает список словарей и выводит новый список словарей, по указанному ключу 'state',
-        если ключ не был дан на вход функции, будет выведен список по ключу по умолчанию"""
+    если ключ не был дан на вход функции, будет выведен список по ключу по умолчанию"""
     new_list = []
-    if status_for_key == 'EXECUTED':
-        for state in list_of_dicts:
-            if state.get('state') == 'EXECUTED':
-                new_list.append(state)
-        return new_list
-    elif status_for_key == 'CANCELED':
-        for state in list_of_dicts:
-            if state.get('state') == 'CANCELED':
-                new_list.append(state)
-        return new_list
+    for state in list_of_dicts:
+        if state.get("state") == status_for_key:
+            new_list.append(state)
+    return new_list
 
 
-def sort_by_date(list_of_dicts: list, ascending: bool= True) -> list:
+def sort_by_date(list_of_dicts: list, ascending: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
-        (по умолчанию - убывание) и возвращает новый список отсортированный по дате"""
-    new_list = [sorted(list_of_dicts, key=lambda date: date.get('date', 0), reverse=ascending)]
+    (по умолчанию - убывание) и возвращает новый список отсортированный по дате"""
+    new_list = [sorted(list_of_dicts, key=lambda date: date.get("date", 0), reverse=ascending)]
     return new_list
