9d14fb33ee6e25f6f3db123b6f7ff9b6d0df6710
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-27 20:02:36 +0300

    fixes with linters

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,30 @@
-def filter_by_currency(transactions: list, currency: str) -> list:
-        result = list(filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions))
-        yield result
+def filter_by_currency(transactions: list, currency: str) -> iter:
+    """
+    Функция принимает на вход список словарей, представляющих транзакции
+        и возвращает итератор, который поочередно выводит
+    транзакции, где валюта операции соответствует заданной
+    """
+    result = list(filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions))
+    yield result
 
 
-def transaction_descriptions( transactions: list) -> str:
+def transaction_descriptions(transactions: list) -> iter:
+    """
+    Генератор, который принимает список словарей с транзакциями
+     и возвращает описание каждой операции по очереди
+    """
     result = map(lambda x: x["description"], transactions)
     for i in result:
         yield i
 
 
-def card_number_generator(start: int, stop: int) -> str:
+def card_number_generator(start: int, stop: int) -> iter:
+    """
+        Генератор, который выводит номера банковских карт в формате
+    XXXX XXXX XXXX XXXX, где X — цифра номера карты. Генератор может сгенерировать номера карт
+    в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генератор должен принимать начальное и конечное значения для генерации диапазона номеров.
+    """
     card_number = ""
     for i in range(start, stop):
         card_number = "0" * (16 - len(str(i))) + str(i)
