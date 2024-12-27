daf00b0045d03bae28a809b3d0f009b4a8da1f21
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2024-12-27 19:38:50 +0300

    create tests in  test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -3,20 +3,10 @@ def filter_by_currency(transactions: list, currency: str) -> list:
         yield result
 
 
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
-
-
 def transaction_descriptions( transactions: list) -> str:
     result = map(lambda x: x["description"], transactions)
     for i in result:
         yield i
-
-
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
 
 
 def card_number_generator(start: int, stop: int) -> str:
@@ -24,7 +14,3 @@ def card_number_generator(start: int, stop: int) -> str:
     for i in range(start, stop):
         card_number = "0" * (16 - len(str(i))) + str(i)
         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}"
-
-
-for card_number in card_number_generator(1, 5):
-    print(card_number)
