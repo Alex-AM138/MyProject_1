7d16f1e7a473039fccf1a572104349c4eacaed2e
Aleksandr Melnikov <Melnikbtw@yandex.ru>
2025-01-03 19:05:19 +0300

    wright decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+import logging
+
+
+def log(func, *, filename=0):
+     if func is None:
+         return lambda func: log(func, filename=filename)
+     logger = logging.getLogger(func.__name__)
+     logger.setLevel(logging.INFO)
+     handler = logging.FileHandler(filename) if filename else logging.StreamHandler()
+     formatter = logging.Formatter('%(message)s')
+     handler.setFormatter(formatter)
+     logger.addHandler(handler)
+     def wrapper(*args, **kwargs):
+         try:
+             logger.info(f"Вызов функции '{func.__name__}' с аргументами {args} и ключевыми аргументами {kwargs}")
+             result = func(*args, **kwargs)
+             logger.info(f"Функция '{func.__name__} OK")
+             return result
+         except Exception as n:
+             logger.error(f"Функция '{func.__name__}' error: {type(n).__name__}. Inputs: {args}, {kwargs}")
+             raise
+     return wrapper
+
+
+@log
+def my_function(x, y):
+    return x + y
+
+
+@log
+def divide(a, b):
+    return a / b
+
+# Тестирование
+my_function(1, 2)  # Успешный вызов
+try:
+    divide(10, 0)  # Ошибка (деление на ноль)
+except ZeroDivisionError:
+    pass
